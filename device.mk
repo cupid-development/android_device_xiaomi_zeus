0965a0c870375f3172e977ec407ea44bae0d133b
Arian <arian.kulmer@web.de>
2022-10-23 23:48:15 +0900

    zeus: Initial bringup
    Based on cupid-development/device_xiaomi_cupid@481fdeede303e476f5eaa8d675b1408a1836baf4
    
    Change-Id: I96c0a38207f20e862ff3770ea5269bafd26563fd
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Roshan Pius <rpius@google.com>
    Co-Authored-By: Cosmin Tanislav <demonsingur@gmail.com>
    Co-Authored-By: Ayush Dubey <ayushdubey70@gmail.com>
    Co-Authored-By: Akito Mizukito <akito@evolution-x.org>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

---
+++ device.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8450-common
+$(call inherit-product, device/xiaomi/sm8450-common/common.mk)
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/zeus/zeus-vendor.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+
+PRODUCT_PACKAGES += \
+    FrameworksResZeus \
+    SettingsResZeus \
+    SystemUIResZeus \
+    WifiResZeus
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
