f90d21741ec082d7653edadde965a75c1583b754
Arian <arian.kulmer@web.de>
2023-05-11 17:53:50 +0200

    zeus: Initial device tree
    Change-Id: I6d195d295b5f94b71c3ff32b21aa33d3af7bf588

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zeus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zeus-user \
+    lineage_zeus-userdebug \
+    lineage_zeus-eng
---
+++ lineage_zeus.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from zeus device
+$(call inherit-product, device/xiaomi/zeus/device.mk)
+
+# Inherit from common lineage configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_zeus
+PRODUCT_DEVICE := zeus
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := 2201122G
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
