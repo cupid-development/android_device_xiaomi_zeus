81a22abeff3214a068585af2c393006732a5885a
Arian <arian.kulmer@web.de>
2024-07-04 12:49:01 +0200

    zeus: Update blobs from V816.0.6.0.ULBEUXM
    Change-Id: Icc45db6c57716c39f1b352b5542d6903358f8ed7

--- lineage_zeus.mk
+++ lineage_zeus.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,14 +16,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2201122G
 
-PRODUCT_SYSTEM_NAME := zeus_global
+PRODUCT_SYSTEM_NAME := zeus_eea
 PRODUCT_SYSTEM_DEVICE := zeus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="zeus_global-user 13 TKQ1.220807.001 V14.0.7.0.TLBMIXM release-keys" \
+    PRIVATE_BUILD_DESC="zeus_eea-user 14 UKQ1.230917.001 V816.0.6.0.ULBEUXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Xiaomi/zeus_global/zeus:13/TKQ1.220807.001/V14.0.7.0.TLBMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/zeus_eea/zeus:14/UKQ1.230917.001/V816.0.6.0.ULBEUXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
