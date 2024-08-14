ffd6a221be7f71e63dba16dd2ebe558b74f3d0d0
Arian <arian.kulmer@web.de>
2024-08-14 13:59:44 +0200

    dagda: Commonize sensors.xiaomi
    Change-Id: I5bd0cc3858677c292814e2dc72438864a29ffecb

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ PRODUCT_PACKAGES += \
     WifiResZeus
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.xiaomi:64
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
